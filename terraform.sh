6c6cf379a454c3fc3d396a59e57df52b8264f213
Tarique-B-DevOps <tariqueatwork@gmail.com>
2025-02-09 15:43:10 +0000

    prompt fix

--- terraform.sh
+++ terraform.sh
@@ -12,7 +12,7 @@ check_terraform() {
 install_terraform() {
   echo "Terraform is not installed. Installing now..."
   
-  wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
